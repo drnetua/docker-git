fc479b3cacd0df77f79df080b9a4965a7e377109
gusa@ciklum.com <git5780057>
2018-07-29 06:40:33 +0300

    commit

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t intoxicate/docker-git:git-2.5.1-cpu-python3.5 cpu-based/ -f
-docker build -t intoxicate/docker-git:git-2.5.1-gpu-python3.5-cuda9.0-cudnn7.0 gpu-based/
+docker build -t intoxicate/docker-git:git-2.5.1-cpu-python3.5 -f cpu-based/Dockerfile context
+docker build -t intoxicate/docker-git:git-2.5.1-gpu-python3.5-cuda9.0-cudnn7.0 gpu-based/Dockerfile context
