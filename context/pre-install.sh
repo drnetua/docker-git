0585f30f9e1c3e18e85fe9a42895ff6f0e384baa
gusa@ciklum.com <git5780057>
2018-07-29 07:11:13 +0300

    commit

---
+++ context/pre-install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+/root/anaconda3/bin/pip install --upgrade pip
+
+/root/anaconda3/bin/pip install flask flask_restful cntk opencv-python
+
+apt-get install -y build-essential cmake pkg-config \
+    libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev \
+    libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
+    libxvidcore-dev libx264-dev libsm6 libxext6
+
+apt-get install -y libxrender-dev
+
+
+
+
+
+
